e8a18a83c17e5b73ba43cdfd510e29a5cd276324
Dieter Heidrich <dnbheidrich@gmail.com>
2020-01-29 15:21:42 -0700

    changes

--- app.js
+++ app.js
@@ -5,32 +5,23 @@ let rockElem = document.getElementById("btnRock")
 let paperElem = document.getElementById("btnPaper")
 let scissorElem = document.getElementById("btnScissors")
 
-function pickRock(){
+function playerChoice(){
  if (choice[0]) {
    console.log("Rock!")
  }
 
-}
-function pickPaper(){
-   if (choice[1])
+ else if (choice[1]){
    console.log("Paper")
 
-}
-function pickScissors(){
-  if (choice[2])
+}else
+  if (choice[2]){
   console.log("Scissors!")
 
+}
 }
 
 function compChoice(){
 Math.floor(Math.random() * choice.length)
 }
-function drawGame() {
-  
-  document.getElementById("btnRock");
-  document.getElementById("btnPaper");
-  document.getElementById("btnScissors")
- 
-  drawGame()
-}
+
 
