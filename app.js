6c763e79e9e5ad5562207ac071add4e8dbbea57a
Dieter Heidrich <dnbheidrich@gmail.com>
2020-01-29 16:32:22 -0700

    changes

--- app.js
+++ app.js
@@ -1,27 +1,58 @@
 
-let choice = ["Rock", "Paper", "Scissors"]
-
+let computerChoice = ["Rock", "Paper", "Scissors"]
+let playerchoice = ["Rock", "Paper", "Scissors"]
 let rockElem = document.getElementById("btnRock")
 let paperElem = document.getElementById("btnPaper")
 let scissorElem = document.getElementById("btnScissors")
 
-function playerChoice(){
- if (choice[0]) {
-   console.log("Rock!")
- }
+function pickRock(){
+  let randoChoice = Math.floor(Math.random() * computerChoice.length)
+  if (playerchoice[0] == computerChoice[randoChoice]) { 
+    console.log("Tie")
+  
+  } 
+  else if(playerchoice[0] == computerChoice[randoChoice]){
+    console.log("Lose")
+  } else {
+    console.log("You Win")
+  }
+  }
+  
 
- else if (choice[1]){
-   console.log("Paper")
+function pickPaper(){
+  let randoChoice = Math.floor(Math.random() * computerChoice.length)
+ if (playerchoice[1] == computerChoice[randoChoice]) { 
+    console.log("You win")
 
-}else
-  if (choice[2]){
-  console.log("Scissors!")
-
+    
+  } else if(playerchoice[1] == computerChoice[randoChoice]){
+    console.log("Tie")
+  } else {
+    console.log("You Lose")
+  }
 }
+function pickScissors(){
+  let randoChoice = Math.floor(Math.random() * computerChoice.length)
+  if (playerchoice[2] == computerChoice[randoChoice]) { 
+    console.log("You Lose")
+    debugger
+    
+  } else if(playerchoice[2] == computerChoice[randoChoice]){
+    console.log("You Win")
+  } else {
+    console.log("Tie")
+  } 
 }
 
-function compChoice(){
-Math.floor(Math.random() * choice.length)
-}
+
+
+
+
+
+
+
+
+
+
 
 
