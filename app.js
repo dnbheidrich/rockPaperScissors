24f1693865fc33d7a825176cc916011f0fdef453
Dieter Heidrich <dnbheidrich@gmail.com>
2020-01-29 15:04:16 -0700

    changes

---
+++ app.js
@@ -0,0 +1,28 @@
+
+let choice = ["Rock", "Paper", "Scissors"]
+
+let rockElem = document.getElementById("btnRock")
+let paperElem = document.getElementById("btnPaper")
+let scissorElem = document.getElementById("btnScissors")
+
+function pickRock(){
+ if (choice[0]) {
+   console.log("Rock!")
+ }
+
+}
+function pickPaper(){
+   if (choice[1])
+   console.log("Paper")
+
+}
+function pickScissors(){
+  if (choice[2])
+  console.log("Scissors!")
+
+}
+
+function compChoice(){
+Math.floor(Math.random() * choice.length)
+}
+
